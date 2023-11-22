6b1fb500dac8433ca062ec6313eb519c4670dc25
Omar Abdelhadi <omarabdelhadi@cmail.carleton.ca>
2023-11-21 23:46:00 -0500

    Initial project set up, base code

---
+++ build-3004project-Qt_6_6_0_for_macOS-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/omarabdelhadi/Desktop/COMP 3004/project/COMP-3004/build-3004project-Qt_6_6_0_for_macOS-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/omarabdelhadi/Qt/6.6.0/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/omarabdelhadi/Qt/6.6.0/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
